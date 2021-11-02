f4b533fef42e8edc525a4c3082f26cde1776e263
danilsdf <48866421+danilsdf@users.noreply.github.com>
2021-11-02 23:00:01 +0200

    Update GoogleSettings.cs

--- Core/Google/GoogleSettings.cs
+++ Core/Google/GoogleSettings.cs
@@ -8,6 +8,6 @@ namespace BackParse.Core.Google
     {
         public string BaseUrl { get; set; } = @"https://google-search3.p.rapidapi.com/api/v1/search/q=*Name*+4pda";
         public string Rapidapi_host { get; set; } = "google-search3.p.rapidapi.com";
-        public string Rapidapi_key { get; set; } = "b0022d0c7amshee6f4d997552368p1b7606jsn60d2f2d806bf";
+        public string Rapidapi_key { get; set; } = "ApiKey";
     }
 }
